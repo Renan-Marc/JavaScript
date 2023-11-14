64b249ce4f982eddea8cd4b7d91e8f876a7cde24
Renan Romão <147003162+Renan-Marc@users.noreply.github.com>
2023-11-14 19:52:36 -0300

    Novos exercícios js

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 20
+        console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+
+    if (vel > 60) { //Condição simples!
+        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+    }
+
+        console.log(`Dirija sempre usando o cinto de segurança`)
+
