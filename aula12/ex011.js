a1b0d1fb44b9ed175da78fac02a76edd022165f7
Renan Romão <147003162+Renan-Marc@users.noreply.github.com>
2023-11-23 20:25:12 -0300

    Aninhamento do código

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,12 +1,10 @@
-var agora = new Date()
-var hora = agora.getHours
-console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 12){
-    console.log(`Bom Dia!`)
-}else if (hora <= 18){
-        console.log(`Boa Tarde!`)
-}else{
-        console.log(`Boa Noite!`)
+var idade = 11
+console.log(`Você tem ${idade} anos`)
+if (idade < 16){
+        console.log(`Não vota`)
+} else if(idade < 18 || idade > 65){
+        console.log(`Voto opcional`)
+}else {
+        console.log(`Voto obrigatorio`)
 }
-
 
